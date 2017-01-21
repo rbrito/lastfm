cea6ddfb6c9343b719d87e2a43c837b5ab7fcac8
Rogério Brito <rbrito@ime.usp.br>
2017-01-21 02:46:17 -0200

    lastfm: Make the code slightly more general.
    And hugely more complex. Not sure if it was worth it, as it it
    "enterprise-y".

--- lastfm.py
+++ lastfm.py
@@ -1,15 +1,45 @@
 #!/bin/env/python3
 
 import json
+import urllib.parse
 import urllib.request
 
-url = 'http://ws.audioscrobbler.com/2.0/?api_key=b25b959554ed76058ac220b7b2e0a026&format=json'
+_url = 'http://ws.audioscrobbler.com/2.0/?'
+
+_params = {
+    'api_key': 'b25b959554ed76058ac220b7b2e0a026',
+    'format': 'json',
+}
+
 
 def get_similar_artists_url(artist, limit=5):
-    return url + '&method=artist.getSimilar' + '&artist=' + artist + '&limit=' + str(limit)
+    used_params = {
+        'method': 'artist.getSimilar',
+        'artist': artist,
+        'limit': limit,
+    }
+
+    # To avoid using Python 3.5's syntax of {**used_params, **_params}
+    used_params.update(_params)
+    query_string = urllib.parse.urlencode(used_params)
+
+    return _url + query_string
+
 
 def get_similar_tracks_url(artist, track, limit=5):
-    return url + '&method=track.getSimilar' + '&artist=' + artist + '&track=' + track + '&limit=' + str(limit)
+    used_params = {
+        'method': 'track.getSimilar',
+        'artist': artist,
+        'track': track,
+        'limit': limit,
+    }
+
+    # To avoid using Python 3.5's syntax of {**used_params, **_params}
+    used_params.update(_params)
+    query_string = urllib.parse.urlencode(used_params)
+
+    return _url + query_string
+
 
 def get_json(url):
     with urllib.request.urlopen(url) as f:
