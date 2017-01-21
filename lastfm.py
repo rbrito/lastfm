71b07e0350b32d3f9cd548aae682d3245e8908df
Rogério Brito <rbrito@ime.usp.br>
2017-01-21 01:29:51 -0200

    lastfm: Initial toy to query last.fm.

---
+++ lastfm.py
@@ -0,0 +1,24 @@
+#!/bin/env/python3
+
+import json
+import urllib.request
+
+url = 'http://ws.audioscrobbler.com/2.0/?api_key=b25b959554ed76058ac220b7b2e0a026&format=json'
+
+def get_similar_artists_url(artist, limit=5):
+    return url + '&method=artist.getSimilar' + '&artist=' + artist + '&limit=' + str(limit)
+
+def get_similar_tracks_url(artist, track, limit=5):
+    return url + '&method=track.getSimilar' + '&artist=' + artist + '&track=' + track + '&limit=' + str(limit)
+
+def get_json(url):
+    with urllib.request.urlopen(url) as f:
+        data_str = f.read().decode()
+
+    data = json.loads(data_str)
+    return data
+
+artists = get_json(get_similar_artists_url('opeth'))['similarartists']['artist']
+
+for artist in artists:
+    print('Artist: %s, Similarity: %.2f%%' %(artist['name'], 100 * float(artist['match'])))
